7cbf50f85296f5b2392327cade071037ca73321e
Nate-Owen <nate.janti@gmail.com>
2025-05-28 02:57:33 +0000

    amazing

--- git/gitgoing/__init__.py
+++ git/gitgoing/__init__.py
@@ -1,4 +1,10 @@
 # this is a smaall package to help you with git
+
+gitgoing = "0.1.0"
+__version__ = gitgoing
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
+
 from .gitgoing import (
     gitgoing,
     gitgoing_init,
