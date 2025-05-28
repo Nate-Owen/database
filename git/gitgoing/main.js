6d7035924036c8b610a080a0b69e3d3340d05920
Nate-Owen <nate.janti@gmail.com>
2025-05-28 14:36:56 +0000

    great

---
+++ git/gitgoing/main.js
@@ -0,0 +1,9 @@
+ branch: main
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import './assets/css/global.css';
+import { createPinia } from 'pinia';
