9e91a0f26c5fb3b9d7f34316a7d6445e61095de8
Nate-Owen <nate.janti@gmail.com>
2025-05-28 02:56:01 +0000

    awesome

--- git/gitgoing/__init__.py
+++ git/gitgoing/__init__.py
@@ -1,1 +1,14 @@
-# This file is intentionally left blank.
+# this is a smaall package to help you with git
+from .gitgoing import (
+    gitgoing,
+    gitgoing_init,
+    gitgoing_status,
+    gitgoing_commit,
+    gitgoing_push,
+    gitgoing_pull,
+    gitgoing_clone,
+    gitgoing_branch,
+    gitgoing_checkout, 
+    gitgoing_merge,
+    gitgoing_rebase,
+)
