f39988409b35bc0f48d2455986d250fb9817e891
Nate-Owen <nate.janti@gmail.com>
2025-05-29 01:08:04 +0000

    great

--- git/gitgoing/main.js
+++ git/gitgoing/main.js
@@ -7,3 +7,12 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import './assets/css/global.css';
 import { createPinia } from 'pinia';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+app.use(ElementPlus);
+app.use(createPinia());
+
+app.mount('#app');
