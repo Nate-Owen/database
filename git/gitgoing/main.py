728153fa73191046866f382deef451ff19a521c0
Nate-Owen <nate.janti@gmail.com>
2025-05-30 03:23:55 +0000

    yes

--- git/gitgoing/main.py
+++ git/gitgoing/main.py
@@ -14,3 +14,10 @@ for i, value in enumerate(range(10)):
     print(f"Iteration {i}: {value}")
 
     i += 1
+
+    vars = locals()
+# This is a simple loop that prints the iteration number and value.
+# The loop iterates over a range of 10, printing the current iteration and value.
+# The `i` variable is incremented by 1 in each iteration.
+# The `vars` variable is assigned the local variables of the current scope.
+# The `locals()` function is used to get the local variables in the current scope.
